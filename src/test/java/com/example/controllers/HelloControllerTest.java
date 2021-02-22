aebaed7c17c9a46378409df3c594781a7ff94861
kriogenia <esekriogenia@gmail.com>
2021-02-22 17:27:51 +0100

    Hello World test

---
+++ src/test/java/com/example/controllers/HelloControllerTest.java
@@ -0,0 +1,30 @@
+package com.example.controllers;
+
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.client.RxHttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@MicronautTest
+public class HelloControllerTest {
+
+    @Inject
+    @Client("/")
+    RxHttpClient client;
+
+    @Test
+    public void testHello() {
+        HttpRequest<String> request = HttpRequest.GET("/hello");
+        String body = client.toBlocking().retrieve(request);
+
+        assertNotNull(body);
+        assertEquals("Hello World", body);
+
+    }
+}
