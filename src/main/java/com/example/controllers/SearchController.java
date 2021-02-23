65dd6695b0a3ac5849ffe965509fe00485a0fa45
kriogenia <esekriogenia@gmail.com>
2021-02-23 11:27:10 +0100

    Add search controller

---
+++ src/main/java/com/example/controllers/SearchController.java
@@ -0,0 +1,32 @@
+package com.example.controllers;
+
+import com.example.pojos.SearchResult;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.PathVariable;
+import io.micronaut.http.annotation.Produces;
+import org.elasticsearch.client.ml.PostDataRequest;
+
+@Controller("/search")
+public class SearchController {
+
+    @Get(value = "/{query}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String searchQuery(@PathVariable String query) {
+        ObjectMapper mapper = new ObjectMapper();
+        String json = null;
+        try {
+            json = mapper.writeValueAsString(new SearchResult(query, "version"));
+            return json;
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+}
---
+++ src/main/java/com/example/pojos/SearchResult.java
@@ -0,0 +1,29 @@
+package com.example.pojos;
+
+public class SearchResult {
+
+	private String query;
+	private String cluster_name;
+
+	public SearchResult(String query, String cluster_name) {
+		this.query = query;
+		this.cluster_name = cluster_name;
+	}
+
+	public String getQuery() {
+		return query;
+	}
+
+	public void setQuery(String query) {
+		this.query = query;
+	}
+
+	public String getCluster_name() {
+		return cluster_name;
+	}
+
+	public void setCluster_name(String cluster_name) {
+		this.cluster_name = cluster_name;
+	}
+
+}
